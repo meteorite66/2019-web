d22584cd5b473bb3a1733af4f1d5dbea6ca9cf84
meteorite66 <takumi5210@gmail.com>
2021-01-18 14:58:28 +0900

    トリボッチ数

---
+++ Chapter3/01-node/02.js
@@ -0,0 +1,63 @@
+'use strict'
+
+
+// function fib(n) {
+//     if (n === 0)
+//         return 0;
+//     else if (n === 1)
+//         return 1;
+//     else {
+
+//     }
+//     return fib(n-1) + fib(n-2);
+// }
+// const length = 40;
+// for (let i = 0; i <= length; i++)
+//     console.log(fib(i))
+
+// let memo = new Map();
+
+// memo.set(0, 0);
+// memo.set(1, 0);
+
+
+// function fib(n) {
+//     if (memo.has(n)) {
+//         return memo.get(n);
+//     }
+
+
+//     const value = fib(n - 1) + fib(n - 2);
+//     memo.set(n, value)
+//     return value;
+// }
+
+// const length = 40;
+
+// for (let i = 0; i<=length; i++){
+//     console.log(fib(i))
+// }
+
+const memo = new Map();
+
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+
+function tri(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    
+    const result = tri(n - 1) + tri(n - 2) + tri(n - 3);
+
+    memo.set(n, result)
+
+    return memo.get(n)
+}
+
+const length = 10;
+
+for (let i = 0; i <= length; i++) {
+    console.log(`${i}: `+tri(i));
+}
